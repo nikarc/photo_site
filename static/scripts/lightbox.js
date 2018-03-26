253dbd1c03b563e483ca52f6097d8d976ddaccf0
Nick Arcuri <narcuri1@gmail.com>
2018-03-25 21:08:15 -0400

    Add images, fix lightbox

--- static/scripts/lightbox.js
+++ static/scripts/lightbox.js
@@ -3,100 +3,119 @@ $(function() {
     var self = this;
     self.images = options.images;
 
-    var currentIndex = options.startIndex || 0;
+    self.parent = options.el;
 
-    var html = '<div class="lightbox" style="display: none;">'+
+    self.currentIndex = options.startIndex || 0;
+
+    var lightboxLength = $('.lightbox').length;
+    var lightboxId = lightboxLength + 1;
+    self.lightboxId = 'lightbox' + lightboxId;
+    var _lightboxId = '#' + self.lightboxId;
+
+    var html = '<div id="' + self.lightboxId + '" class="lightbox" style="display: none;">'+
                   '<div class="lightbox__close-wrap">'+
                     '<a href="#" class="lightbox__close"><i class="fa fa-times"></i></a>'+
                   '</div>'+
                   '<div class="lightbox__wrap">'+
-                    '<a href="#" class="lightbox__prev"><i class="fa fa-chevron-left"></i></a>'+
-                    '<img class="lightbox__image" src="" alt="" />'+
-                    '<a href="#" class="lightbox__next"><span class="fa fa-chevron-right"></span></a>'+
+                    '<a href="#" class="lightbox__controls lightbox__prev"><i class="fa fa-chevron-left"></i></a>'+
+                    '<a href="" target="_blank" class="lightbox__image-link">'+
+                      '<img class="lightbox__image" src="" alt="" />'+
+                    '</a>'+
+                    '<a href="#" class="lightbox__controls lightbox__next"><span class="fa fa-chevron-right"></span></a>'+
                   '</div>'+
                 '</div>';
 
     $('body').append(html);
 
-    var imageEl = $('.lightbox__image');
+    self.imageEl = $(_lightboxId + ' .lightbox__image');
 
-    imageEl.attr('src', self.images[0]);
+    self.imageEl.attr('src', self.images[0]);
 
-    self.nextImage = function() {
-      if (currentIndex === self.images.length - 1) {
-        currentIndex = 0;
-      } else {
-        currentIndex++;
-      }
-
-      imageEl.animate({
-        opacity: 0
-      }, 300);
-      setTimeout(function() {
-        imageEl.attr('src', '');
-        imageEl.attr('src', self.images[currentIndex]);
-        imageEl.animate({
-          opacity: 1
-        }, 300);
-      }, 300);
-    };
-
-    self.previousImage = function() {
-      if (currentIndex === 0) {
-        currentIndex = self.images.length - 1;
-      } else {
-        currentIndex--;
-      }
-
-      imageEl.animate({
-        opacity: 0
-      }, 300);
-      setTimeout(function() {
-        imageEl.attr('src', '');
-        imageEl.attr('src', self.images[currentIndex]);
-        imageEl.animate({
-          opacity: 1
-        }, 300);
-      }, 300);
-    };
-
-    $('.lightbox__next').click(function(e) {
+    $(_lightboxId + ' .lightbox__next').click(function(e) {
       e.preventDefault();
       self.nextImage();
     });
-    $('.lightbox__prev').click(function(e) {
+    $(_lightboxId + ' .lightbox__prev').click(function(e) {
       e.preventDefault();
       self.previousImage();
     });
 
     self.show = function(index) {
-      if (self.images[index]) {
-        imageEl.attr('src', self.images[index]);
+      var image = self.images[index];
+      if (image) {
+        self.imageEl.attr('src', image);
+        self.imageEl.parent('.lightbox__image-link').attr('href', image);
       }
-      $('.lightbox').fadeIn(300);
+
+      $(_lightboxId).fadeIn(300);
       $('body').css('overflow', 'hidden');
     };
 
     self.hide = function() {
-      $('.lightbox').fadeOut(300);
+      console.log($(_lightboxId + ' .lightbox'));
+      $(_lightboxId).fadeOut(300);
       $('body').css('overflow', 'auto');
     };
 
-    $('.lightbox__close').click(function(e) {
+    $(_lightboxId + ' .lightbox__close').click(function(e) {
       e.preventDefault();
       self.hide();
     });
 
-    $('.lightbox__open').click(function(e) {
+    $(self.parent + ' .lightbox__open').click(function(e) {
       e.preventDefault();
-      imageEl.attr('src', '');
+      self.imageEl.attr('src', '');
       var index = $(this).data('lightbox-index');
       if (index) {
         index = parseInt(index, 10);
-        currentIndex = index;
+        self.currentIndex = index;
       }
 
       self.show(index || 0);
     });
+  };
+
+  Lightbox.prototype.nextImage = function() {
+    var self = this;
+    if (self.currentIndex === self.images.length - 1) {
+      self.currentIndex = 0;
+    } else {
+      self.currentIndex++;
+    }
+
+    // var imageEl = $('#' + self.lightboxId + ' .lightbox__image');
+
+    self.imageEl.animate({
+      opacity: 0
+    }, 300);
+    setTimeout(function() {
+      self.imageEl.attr('src', '');
+      self.imageEl.attr('src', self.images[self.currentIndex]);
+      self.imageEl.parent('.lightbox__image-link').attr('href', self.images[self.currentIndex]);
+      self.imageEl.animate({
+        opacity: 1
+      }, 300);
+    }, 300);
+  };
+
+  Lightbox.prototype.previousImage = function() {
+    var self = this;
+    if (self.currentIndex === 0) {
+      self.currentIndex = self.images.length - 1;
+    } else {
+      self.currentIndex--;
+    }
+
+    self.imageEl.animate({
+      opacity: 0
+    }, 300);
+    setTimeout(function() {
+      self.imageEl.attr('src', '');
+      self.imageEl.attr('src', self.images[self.currentIndex]);
+      self.imageEl.parent('.lightbox__image-link').attr('href', self.images[self.currentIndex]);
+      self.imageEl.animate({
+        opacity: 1
+      }, 300);
+    }, 300);
   };
 });
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,11 +1,40 @@
 $(function() {
-  var lightbox = new Lightbox({
+  var blackWhiteLightbox = new Lightbox({
+    el: '#black-and-white',
     images: [
       'static/images/tempImageForSave 2.JPG',
       'static/images/tempImageForSave 5.JPG',
       'static/images/tempImageForSave 7.JPG',
       'static/images/tempImageForSave 8.JPG',
-      'static/images/tempImageForSave 9.JPG'
+      'static/images/tempImageForSave 9.JPG',
+      'static/images/site_image1.JPG',
+      'static/images/site_image3.JPG',
+      'static/images/site_image4.JPG',
+      'static/images/site_image5.JPG',
+      'static/images/site_image6.JPG',
     ]
   });
+
+  var colorLightbox = new Lightbox({
+    el: '#color',
+    images: [
+      'static/images/site_image2.jpg',
+      'static/images/site_image7.jpg',
+      'static/images/tempImageForSave 6.jpg',
+      'static/images/site_image8.jpg',
+      'static/images/site_image9.jpg',
+    ]
+  });
+
+  $('.image-list')
+    .on('mouseenter', function() {
+      $(this).prev('.click-to-enlarge').animate({
+        'opacity': 1
+      }, 300);
+    })
+    .on('mouseleave', function() {
+      $(this).prev('.click-to-enlarge').animate({
+        'opacity': 0
+      }, 300);
+    });
 });
